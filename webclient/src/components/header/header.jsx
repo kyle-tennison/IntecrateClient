44d13f6e317b4ff347f95446b3323a30b22c59ea
Kyle Tennison <kyletennison05@gmail.com>
2023-09-01 23:42:50 -0700

    use home page

---
+++ webclient/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import "./header.css"
+
+
+export default function Header(){
+
+    return(
+        <div id="header">
+            <img class="logo icon" src="../../../public/icon.svg"/>
+        </div>
+    )
+
+}
--- webclient/src/containers/home-page/home.jsx
+++ webclient/src/containers/home-page/home.jsx
@@ -1,10 +1,12 @@
 import "./home.css"
+import Header from "../../components/header/header"
 
 export default function Home(){
     return (
-        <>
+        <div id="home-page">
+            <Header />
             <h1>Intecrate</h1>
             <p>Early Beta</p>
-        </>
+        </div>
     )
 }
