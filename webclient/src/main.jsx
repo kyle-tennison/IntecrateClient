5ecccf768c7a47c794200cc3b2026a883be10bb2
Kyle Tennison <kyletennison05@gmail.com>
2023-09-01 23:10:41 -0700

    basic page

--- webclient/src/main.jsx
+++ webclient/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Home from './containers/home-page/home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    intecrate
+    <Home />
   </React.StrictMode>,
 )
