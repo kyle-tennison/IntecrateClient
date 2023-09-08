863f776bc23937a62b356897ef877793c33f7df8
Kyle Tennison <kyletennison05@gmail.com>
2023-09-07 21:50:51 -0700

    fix header style

--- webclient/src/containers/home-page/home.jsx
+++ webclient/src/containers/home-page/home.jsx
@@ -3,14 +3,19 @@ import Header from "../../components/header/header"
 import BlenderAnimation from "./animation"
 
 export default function Home(){
+
+    
+
     return (
         <div id="home-page">
             <div id="animation">
                 <BlenderAnimation></BlenderAnimation>
             </div>
-            <Header />
-            <h1>Intecrate!</h1>
-            <p>Early Beta</p>
+            <div id="home-content">
+                <Header />
+                <h1>Intecrate!</h1>
+                <p>Early Beta</p>
+            </div>
         </div>
     )
 }
