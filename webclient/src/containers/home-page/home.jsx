13a4cedbac73e66693230448abfedb3c0cf59aca
Kyle Tennison <kyletennison05@gmail.com>
2023-09-09 19:24:09 -0700

    add other contents

--- webclient/src/containers/home-page/home.jsx
+++ webclient/src/containers/home-page/home.jsx
@@ -5,6 +5,8 @@ import Fade from "react-reveal/Fade"
 
 
 import man_thinking from "/man-thinking.jpg"
+import laser_cutter from "/laser-cutter.jpg"
+import kid_typing from "/kid-typing.jpg"
 import { useEffect } from "react"
 
 export default function Home(){
@@ -24,8 +26,9 @@ export default function Home(){
                         <div className="home-img half-right">
                             <img src={man_thinking} alt="Someone using intecrate"></img>
                         </div>
-                        <div className="home-paragraph half-left">
-                            &emsp;Intecrate provides video lessons to help you
+                        <div className="home-paragraph half-left" style={{marginTop: "40px"}}>
+                            <strong>You're the designer</strong><br></br>
+                            Intecrate provides video lessons to help you
                             create your own, personal CAD designs. When you're done,
                             we'll connect you with world-class manufactures and 
                             deliver your custom-fabricated aluminum parts for you to
@@ -33,18 +36,36 @@ export default function Home(){
 
                         </div>
                     </div>
-                    <div className="home-block" style={{marginTop : "60px"}}>
+                    <div className="home-block" style={{marginTop : "130px"}}>
                         <div className="home-img half-left">
-                            <img src={man_thinking} alt="Someone using intecrate"></img>
+                            <img src={laser_cutter} alt="Someone using intecrate"></img>
                         </div>
-                        <div className="home-paragraph half-right">
-                            &emsp;Intecrate provides video lessons to help you
-                            create your own, personal CAD designs. When you're done,
-                            we'll connect you with world-class manufactures and 
-                            deliver your custom-fabricated aluminum parts for you to
-                            assembly.
+                        <div className="home-paragraph half-right" style={{marginTop: "50px"}}>
+                            <strong>Built by the best</strong><br></br>
+                            We send your designs out to Reno, Nevada, 
+                            where our friends at <a href="https://sendcutsend.com" target="_blank">SendCutSend</a> have 
+                            12kW laser-cutters waiting to fabricate your designs
+                            at an industry-grade precision. 
 
                         </div>
+                    </div>
+                    <div className="home-block" style={{marginTop : "130px"}}>
+                        <div className="home-img half-right">
+                            <img src={kid_typing} alt="Someone using intecrate"></img>
+                        </div>
+                        <div className="home-paragraph half-left" style={{marginTop: "20px"}}>
+                            <strong>We're here for you</strong><br></br>
+                            We know that taking this next step in your engineering
+                            journey is daunting, but we're here to assist you
+                            regardless of your skill level. We've prepared hundreds
+                            of videos and articles to help you through every problem
+                            you can run into.
+
+                        </div>
+                    </div>
+                    <h1 style={{marginTop : "200px"}}>Everything coming soon</h1>
+                    <div style={{textAlign: "center"}}>
+                        <button id="blog-button">Read the Development Blog</button>
                     </div>
                 </Fade>
                 <Header />  {/* Put header last so that it renders on top */}
