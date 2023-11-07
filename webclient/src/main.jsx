6a35fd51db72f2004a9e581f0079094de4315831
Kyle Tennison <kyletennison05@gmail.com>
2023-11-06 22:14:12 -0800

    Revert "add popup"
    This reverts commit a5283b0560cb59f71bd3d73fb604209e1de0af23.

--- webclient/src/main.jsx
+++ webclient/src/main.jsx
@@ -9,11 +9,9 @@ import Signup from "./containers/signup-page/signup-page";
 import Login from "./containers/login-page/login-page";
 import About from "./containers/about-page/about-page";
 import Profile from "./containers/profile-page/profile-page";
-import { AlertOverwrite } from "./components/alert-overwrite/alert-overwrite";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AlertOverwrite />
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
