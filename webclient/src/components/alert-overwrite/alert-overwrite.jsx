613a122d5a7f6af82663106eebf26239d453df9a
Kyle Tennison <kyletennison05@gmail.com>
2023-11-06 22:29:15 -0800

    trigger on api

--- webclient/src/components/alert-overwrite/alert-overwrite.jsx
+++ webclient/src/components/alert-overwrite/alert-overwrite.jsx
@@ -3,21 +3,23 @@ import "./alert-overwrite.css";
 import React from "react";
 import { useEffect, useState } from "react";
 
-import bug_icon from "./assets/bug.svg";
-
 export function AlertOverwrite() {
   const [error, setError] = useState("");
 
   function showAlert() {
     document.getElementById("alert-box").style.display = "block";
-    document.getElementById("alert-box").style.opacity = 1;
-    // document.getElementById("alert-content").style.opacity = 1;
+
+    // For some reason we need to allow the box to load before it can fade
+    document.getElementById("alert-box").style.opacity = 0;
+    setTimeout(() => {
+      document.getElementById("alert-box").style.opacity = 1;
+    }, 100);
+
   }
   function hideAlert() {
     document.getElementById("alert-box").style.opacity = 0;
-    // document.getElementById("alert-content").style.opacity = 0;
     setTimeout(() => {
-      document.getElementById("alert-box").style.display = "block";
+      document.getElementById("alert-box").style.display = "none";
     }, 300);
   }
 
@@ -43,8 +45,8 @@ export function AlertOverwrite() {
   }, []);
 
   return (
-    <div id="alert-box" style={{ display: "none" }}>
-      <div id="alert-content" style={{ backgroundImage: bug_icon }}>
+    <div id="alert-box" style={{ display: "none"}}>
+      <div id="alert-content" >
         <span
           className="close"
           onClick={() => {
