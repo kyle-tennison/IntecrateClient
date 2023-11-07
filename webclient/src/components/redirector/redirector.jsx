6e5f7c26fe76740e74eddcd04dd4bbb23879d5c1
Kyle Tennison <kyletennison05@gmail.com>
2023-11-06 22:13:43 -0800

    Redo lint; wrong verison
    Revert "Revert "lint""
    
    This reverts commit 91fa3e8c82c208fe0e5af5e58343f864f2e1f32f.

--- webclient/src/components/redirector/redirector.jsx
+++ webclient/src/components/redirector/redirector.jsx
@@ -8,14 +8,13 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
-
 /**
  * Redirects away from a page if logged in / not logged in
- * 
+ *
  * @param {object} props The props object
  * @param {string} props.redirect The path to navigate to
  * @param {boolean} props.onLogin Redirect when logged in (true) or when logged out (false)
- * 
+ *
  */
 export function Redirector(props) {
   const [cookies, setCookie] = useCookies(["api_key"]);
@@ -29,8 +28,8 @@ export function Redirector(props) {
       isLoggedIn = true;
     }
 
-    if (isLoggedIn === props.onLogin){
-        navigate(props.redirect)
+    if (isLoggedIn === props.onLogin) {
+      navigate(props.redirect);
     }
 
     console.log("reading cookies as", cookies.api_key);
